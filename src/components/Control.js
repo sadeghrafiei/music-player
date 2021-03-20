8ddc199faf2c0e59457d8c31497cff2592dd561a
sadeghrafiei <74002994+sadeghrafiei@users.noreply.github.com>
2021-03-20 23:27:03 +0330

    add fontawesome icons

--- src/components/Control.js
+++ src/components/Control.js
@@ -1,12 +1,18 @@
-import { faBackward } from '@fortawesome/free-solid-svg-icons'
+import { faBackward, faForward, faPause } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 
 function Control() {
     return (
         <div className="control">
-            <button>
+            <button className="control__SkipBtn">
                 <FontAwesomeIcon icon={faBackward} />
+            </button>
+            <button className="control__PlayButton">
+                <FontAwesomeIcon icon={faPause} />
+            </button>
+            <button className="control__SkipBtn">
+                <FontAwesomeIcon icon={faForward} />
             </button>
         </div>
     )
