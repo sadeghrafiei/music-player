47b1d506fc14035617a49b93c5ee10bb519fd7f7
sadeghrafiei <74002994+sadeghrafiei@users.noreply.github.com>
2021-03-20 23:27:19 +0330

    add controls in player

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,24 +1,19 @@
-import React from 'react'
-import Details from './Details'
+import React from "react";
+import Control from "./Control";
+import Details from "./Details";
 
 function Player() {
-    return (
-        <div className="player">
-           <h4>
-               Playing Now 
-           </h4>
-           <Details />
-           <audio src="" controls className="player__audio">
-
-           </audio>
-           <p>
-               Next Up: { " " }
-               <span>
-                   Next Song
-               </span>
-           </p>
-        </div>
-    )
+  return (
+    <div className="player">
+      <h4>Playing Now</h4>
+      <Details />
+      <Control />
+      <audio src="" controls className="player__audio"></audio>
+      <p>
+        Next Up: <span>Next Song</span>
+      </p>
+    </div>
+  );
 }
 
-export default Player
+export default Player;
